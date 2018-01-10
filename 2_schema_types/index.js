1b8a691609d4c73fc8d628115b58e24e0e46dafc
allen4dev <alanaliagadev@gmail.com>
2018-01-10 10:31:55 -0400

    [Add] 2: basic type definitions and resolvers

--- 2_schema_types/index.js
+++ 2_schema_types/index.js
@@ -1,5 +1,66 @@
 const express = require('express');
+const axios = require('axios');
+const graphqlHTTP = require('express-graphql');
+const { makeExecutableSchema } = require('graphql-tools');
 
 const app = express();
 
-app.listen(3000, () => console.log('Server running in port 8080'));
+const typeDefs = `
+  type Song {
+    id: Int
+    name: String
+  }
+
+  type Lyric {
+    id: Int
+    content: String
+    song: Song
+  }
+
+  type Query {
+    getSong(id: Int!): Song
+    getLyric(id: Int!): Lyric
+    getSongLyrics(id: Int!): [Lyric]!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    getSong: (parentValue, { id }) => {
+      return axios
+        .get(`http://localhost:3000/songs/${id}`)
+        .then(res => res.data);
+    },
+    getLyric: (parentValue, { id }) => {
+      return axios
+        .get(`http://localhost:3000/lyrics/${id}`)
+        .then(res => res.data);
+    },
+    getSongLyrics: (parentValue, { id }) => {
+      return axios
+        .get(`http://localhost:3000/songs/${id}/lyrics`)
+        .then(res => res.data);
+    },
+  },
+
+  Lyric: {
+    song: parentValue => {
+      return axios
+        .get(`http://localhost:3000/songs/${parentValue.songId}`)
+        .then(res => res.data);
+    },
+  },
+};
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: makeExecutableSchema({
+      typeDefs,
+      resolvers,
+    }),
+    graphiql: true,
+  })
+);
+
+app.listen(8080, () => console.log('Server running in port 8080'));

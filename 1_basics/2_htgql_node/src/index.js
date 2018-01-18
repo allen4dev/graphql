fc24cb13e7760cc2c67ce70ce486744e5fe36848
allen4dev <alanaliagadev@gmail.com>
2018-01-18 18:55:31 -0400

    [Update] minor update

---
+++ 1_basics/2_htgql_node/src/index.js
@@ -0,0 +1,43 @@
+const { GraphQLServer } = require('graphql-yoga')
+const { Graphcool } = require('graphcool-binding')
+
+const resolvers = {
+  Query: {
+    feed(parent, args, ctx, info) {
+      return ctx.db.query.posts({ where: { isPublished: true } }, info)
+    },
+  },
+  Mutation: {
+    createDraft(parent, { title, text }, ctx, info) {
+      return ctx.db.mutation.createPost(
+        // TODO remove `isPublished` in favour of default value
+        { data: { title, text, isPublished: false } },
+        info,
+      )
+    },
+    publish(parent, { id }, ctx, info) {
+      return ctx.db.mutation.updatePost(
+        {
+          where: { id },
+          data: { isPublished: true },
+        },
+        info,
+      )
+    },
+  },
+}
+
+const server = new GraphQLServer({
+  typeDefs: './src/schema.graphql',
+  resolvers,
+  context: req => ({
+    ...req,
+    db: new Graphcool({
+      typeDefs: 'src/generated/graphcool.graphql',
+      endpoint: 'http://localhost:60000/2_htgql_node/dev',
+      secret: 'mysecret123',
+    }),
+  }),
+})
+
+server.start(() => console.log('Server is running on http://localhost:4000'))
